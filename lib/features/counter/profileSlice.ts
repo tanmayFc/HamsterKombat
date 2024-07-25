692949fec7cdc41ffeeeb604030ae2fe96a02187
Sahil-Khan12 <sahil@9039486120@gmail.com>
2024-07-25 17:19:12 +0530

    database functionality

--- lib/features/counter/profileSlice.ts
+++ lib/features/counter/profileSlice.ts
@@ -1,15 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
-export interface profileSliceState {
-  isLogin: boolean
+export interface profileState {
   userId: string
   coins: number
 }
 
-const initialState : profileSliceState = {
-  isLogin: false,
-  userId: '',
+const initialState : profileState = {
+  userId: "",
   coins: 0
 };
 
@@ -17,21 +15,18 @@ export const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    setIsLogin: (state, action : PayloadAction<boolean>) => {
-      state.isLogin = action.payload;
-    },
 
     setUserId: (state, action: PayloadAction<string>) => {
       state.userId = action.payload
     },
 
-    setCoins: (state, action: PayloadAction<string>) => {
-      state.userId = action.payload
+    setCoins: (state, action: PayloadAction<number>) => {
+      state.coins += action.payload
     }
   }
 });
 
-export const { setIsLogin, setUserId, setCoins } =
+export const { setUserId, setCoins } =
   profileSlice.actions;
 
   export default profileSlice.reducer;
